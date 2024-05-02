b7b103589591010cf516dff66072e98bdbb9664a
Maximilian Salén <maximilian@live.se>
2024-05-02 17:22:41 +0200

    bug fix

--- pokergame.py
+++ pokergame.py
@@ -6,7 +6,7 @@ def main():
     qt_app = QApplication(sys.argv)
     player_1 = input("Enter Player 1's name:\n")
     player_2 = input("Enter Player 2's name:\n")
-    starting_money = input("Enter starting money:\n")
+    starting_money = int(input("Enter starting money:\n"))
     game = TexasHoldEm([Player(player_1, starting_money), Player(player_2, starting_money)])
     win = MyWindow(game)
     win.show()
